b5d4a95e697a9eba519c5663498e1e74034994c2
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-02-15 18:50:55 +0700

    first initiate

---
+++ zschool-backend/src/main/java/me/huynhducphu/zschool_backend/ZschoolBackendApplication.java
@@ -0,0 +1,13 @@
+package me.huynhducphu.zschool_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZschoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZschoolBackendApplication.class, args);
+	}
+
+}
---
+++ zschool-backend/src/test/java/me/huynhducphu/zschool_backend/ZschoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.huynhducphu.zschool_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZschoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
